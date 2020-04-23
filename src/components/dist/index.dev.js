47f6047f3c527c0a2ebb10ae61962177bbbade9d
LiuSandy <nsiliushuang@gmail.com>
2020-04-23 18:15:16 +0800

    删除多余代码

---
+++ src/components/dist/index.dev.js
@@ -0,0 +1,31 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Upload", {
+  enumerable: true,
+  get: function get() {
+    return _Upload["default"];
+  }
+});
+Object.defineProperty(exports, "PDF", {
+  enumerable: true,
+  get: function get() {
+    return _PDF["default"];
+  }
+});
+Object.defineProperty(exports, "VirtualizedPdf", {
+  enumerable: true,
+  get: function get() {
+    return _VirtualizedPdf["default"];
+  }
+});
+
+var _Upload = _interopRequireDefault(require("./Upload"));
+
+var _PDF = _interopRequireDefault(require("./PDF"));
+
+var _VirtualizedPdf = _interopRequireDefault(require("./VirtualizedPdf"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Upload } from './Upload'
 export { default as PDF } from './PDF'
 export { default as VirtualizedPdf } from './VirtualizedPdf'
-export { default as FileUpload } from './FileUpload'
